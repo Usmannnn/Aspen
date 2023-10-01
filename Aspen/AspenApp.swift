530af114d1a372768f34f4860939feb2febd6a53
Osman Oruc <osman.oruc@ext.digiturk.com.tr>
2023-10-01 15:34:32 +0300

    Initial Commit

---
+++ Aspen/AspenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AspenApp.swift
+//  Aspen
+//
+//  Created by Osman Oruc on 1.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AspenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
